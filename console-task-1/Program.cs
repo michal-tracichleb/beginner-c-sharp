4621a52082b99203fbcb42da115c090d4fe54832
Michał Tracichleb <Michal.Tracichleb@gmail.com>
2022-08-27 20:39:15 +0200

    Used methods

--- console-task-1/Program.cs
+++ console-task-1/Program.cs
@@ -1,2 +1,39 @@
-﻿string aName = "Michał";
+﻿//string with name
+string aName = "Michał";
 Console.WriteLine($"Hello {aName}!");
+
+// .lenght
+Console.WriteLine("----------------------------------");
+string firstName = "Michał";
+string secondName = "Kaja";
+Console.WriteLine($"My friends are {firstName} and {secondName}");
+
+Console.WriteLine($"The name {firstName} has {firstName.Length} letters");
+Console.WriteLine($"The name {secondName} has {secondName.Length} letters");
+
+//  .TrimStart()   .TrimEnd()   .Trim()
+Console.WriteLine("----------------------------------");
+string helloWorld = "    Hello World!    ";
+Console.WriteLine($"[{helloWorld}]");
+
+string trimedHelloWorld = helloWorld.TrimStart();
+Console.WriteLine($"[{trimedHelloWorld}]");
+
+trimedHelloWorld = helloWorld.TrimEnd();
+Console.WriteLine($"[{trimedHelloWorld}]");
+
+trimedHelloWorld = helloWorld.Trim();
+Console.WriteLine($"[{trimedHelloWorld}]");
+
+// .Replace(A, B)
+Console.WriteLine("----------------------------------");
+string sayHello = "Hello World!";
+Console.WriteLine($"{sayHello}");
+
+sayHello = sayHello.Replace("Hello", "Hi");
+Console.WriteLine($"{sayHello}");
+
+// .ToUpper()   .ToLower()
+Console.WriteLine("----------------------------------");
+Console.WriteLine($"{sayHello.ToUpper()}");
+Console.WriteLine($"{sayHello.ToLower()}");
