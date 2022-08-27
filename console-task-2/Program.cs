871afe3f286c19f3bc57cffd71e2b6f1f05a95be
Michał Tracichleb <Michal.Tracichleb@gmail.com>
2022-08-27 21:56:25 +0200

    Other types and Math. (long dciemal short)

--- console-task-2/Program.cs
+++ console-task-2/Program.cs
@@ -1,6 +1,9 @@
 ﻿// Manipulate integral and floating point numbers in C#
 
 // int
+using System;
+using System.Runtime.InteropServices;
+
 int a = 18;
 int b = 6;
 
@@ -36,6 +39,7 @@ Console.WriteLine($"({a} + {b}) - 6 * {c} + (12 * 4) : 3 + 12 = {d}");
 
 //Explore integer precision and limits
 Console.WriteLine("-------------------------------------------");
+Console.WriteLine("int - precision and limits");
 // Integer division truncates the result
 a = 7;
 b = 4;
@@ -48,15 +52,16 @@ Console.WriteLine($"remainder: {e}");
 
 // Max nad min - int
 Console.WriteLine("-------------------------------------------");
-int max = int.MaxValue;
-int min = int.MinValue;
-Console.WriteLine($"The range of integers is {min} to {max}");
+int maxInt = int.MaxValue;
+int minInt = int.MinValue;
+Console.WriteLine($"The range of integers is {minInt} to {maxInt}");
 
-int what = max + 3;
+int what = maxInt + 3;
 Console.WriteLine($"An example of overflow: {what} (max +3)");
 
 //double
 Console.WriteLine("-------------------------------------------");
+Console.WriteLine("double - precision and limits");
 double aa = 5;
 double bb = 4;
 double cc = 2;
@@ -70,7 +75,7 @@ dd = (aa + bb) / cc;
 Console.WriteLine($"({aa} + {bb}) : {cc} = {dd}");
 
 double third = 1.0 / 3.0;
-Console.WriteLine($"1 : 3 = {third}");
+Console.WriteLine($"Double - 1 : 3 = {third}");
 
 // Max and min - double
 Console.WriteLine("-------------------------------------------");
@@ -78,5 +83,46 @@ double maxDouble = double.MaxValue;
 double minDouble = double.MinValue;
 Console.WriteLine($"The range of double is {minDouble} to {maxDouble}");
 
-// 
+// decimal
 Console.WriteLine("-------------------------------------------");
+Console.WriteLine("decimal - precision and limits");
+
+decimal minDecimal = decimal.MinValue;
+decimal maxDecimal = decimal.MaxValue;
+Console.WriteLine($"The range of the decimal type is {minDecimal} to {maxDecimal}");
+
+decimal aaa = 1.0M;
+decimal bbb = 3.0M;
+decimal thirdDecimal = aaa / bbb;
+Console.WriteLine($"Decimal - 1 : 3 = {thirdDecimal}");
+
+//long
+Console.WriteLine("-------------------------------------------");
+Console.WriteLine("long - precision and limits");
+
+long minLong = long.MinValue;
+long maxLong = long.MaxValue;
+Console.WriteLine($"The range of the long type is {minLong} to {maxLong}");
+
+long aaaa = 1;
+long bbbb = 3;
+long thirdLong = aaaa / bbbb;
+Console.WriteLine($"Long - 1 : 3 = {thirdLong}");
+
+//short
+Console.WriteLine("-------------------------------------------");
+Console.WriteLine("short - precision and limits");
+
+long minShort = short.MinValue;
+long maxShort = short.MaxValue;
+Console.WriteLine($"The range of the short type is {minShort} to {maxShort}");
+
+long aaaaa = 1;
+long bbbbb = 3;
+long thirdShort = aaaaa / bbbbb;
+Console.WriteLine($"Short - 1 : 3 = {thirdShort}");
+
+// area of cirle
+double radius = 2.50;
+double area = Math.PI * radius * radius;
+Console.WriteLine($"PI * 2,5 * 2,5 = {area}");
