c4eafcc30a019deec60f66617485aeb1cc5cffff
Michał Tracichleb <Michal.Tracichleb@gmail.com>
2022-08-27 21:38:10 +0200

    precision and limits - int and double

--- console-task-2/Program.cs
+++ console-task-2/Program.cs
@@ -21,6 +21,7 @@ c = a / b;
 Console.WriteLine($"{a} : {b} = {c}");
 
 // Order of operations
+Console.WriteLine("-------------------------------------------");
 a = 5;
 b = 4;
 c = 2;
@@ -31,10 +32,51 @@ d = (a + b) * c;
 Console.WriteLine($"({a} + {b}) * {c} = {d}");
 
 d = (a + b) - 6 * c + (12 * 4) / 3 + 12;
-Console.WriteLine($"({a} + {b}) - 6 * {c} + (12 * 4) / 3 + 12 = {d}");
+Console.WriteLine($"({a} + {b}) - 6 * {c} + (12 * 4) : 3 + 12 = {d}");
 
+//Explore integer precision and limits
+Console.WriteLine("-------------------------------------------");
+// Integer division truncates the result
 a = 7;
 b = 4;
 c = 3;
 d = (a + b) / c;
-Console.WriteLine($"({a} + {b}) / {c} = {d}");
+int e = (a + b) % c;
+Console.WriteLine($"int: ({a} + {b}) : {c} = {d} - the wrong answer!");
+Console.WriteLine($"quotient: {d}");
+Console.WriteLine($"remainder: {e}");
+
+// Max nad min - int
+Console.WriteLine("-------------------------------------------");
+int max = int.MaxValue;
+int min = int.MinValue;
+Console.WriteLine($"The range of integers is {min} to {max}");
+
+int what = max + 3;
+Console.WriteLine($"An example of overflow: {what} (max +3)");
+
+//double
+Console.WriteLine("-------------------------------------------");
+double aa = 5;
+double bb = 4;
+double cc = 2;
+double dd = (aa + bb) / cc;
+Console.WriteLine($"({aa} + {bb}) : {cc} = {dd}");
+
+aa = 19;
+bb = 23;
+cc = 8;
+dd = (aa + bb) / cc;
+Console.WriteLine($"({aa} + {bb}) : {cc} = {dd}");
+
+double third = 1.0 / 3.0;
+Console.WriteLine($"1 : 3 = {third}");
+
+// Max and min - double
+Console.WriteLine("-------------------------------------------");
+double maxDouble = double.MaxValue;
+double minDouble = double.MinValue;
+Console.WriteLine($"The range of double is {minDouble} to {maxDouble}");
+
+// 
+Console.WriteLine("-------------------------------------------");
