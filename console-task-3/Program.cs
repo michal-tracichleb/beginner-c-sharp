a9f71058ddf19655f50c2bd6d41c74ae8d53961b
Michał Tracichleb <Michal.Tracichleb@gmail.com>
2022-08-28 23:12:00 +0200

    branches and loops

--- console-task-3/Program.cs
+++ console-task-3/Program.cs
@@ -1,2 +1,67 @@
 ﻿// branches-and-loops
-Console.WriteLine("Hello, World!");
+
+using System;
+
+namespace ConsoleAPP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //if statement
+
+            int a = 5;
+            int b = 3;
+            int c = 4;
+            // || - or   && - or   == - equal
+            if ((a + b + c > 10) || (a == b))
+            {
+                Console.WriteLine("The answer is greater than 10");
+                Console.WriteLine("Or the first number is equal to the second");
+            }
+            else
+            {
+                Console.WriteLine("The answer is not greater than 10");
+                Console.WriteLine("And the first number is not equal to the second");
+            }
+
+            Console.WriteLine("==============================================================");
+
+            // loop -> while; do while; for
+
+            int counter = 0;
+            while (counter < 5)
+            {
+                Console.WriteLine($"Hello World! The counter is {counter} (while)");
+                counter++;
+            }
+
+            Console.WriteLine("==============================================================");
+
+            counter = 0;
+            do
+            {
+                Console.WriteLine($"Hello World! The counter is {counter} (do -> while)");
+                counter++;
+            } while (counter < 5);
+
+            Console.WriteLine("==============================================================");
+
+            for (counter = 0; counter < 5; counter++)
+            {
+                Console.WriteLine($"Hello World! The counter is {counter} (for)");
+            }
+
+            Console.WriteLine("==============================================================");
+
+            for (int row = 1; row < 5; row++)
+            {
+                for (char column = 'a'; column < 'e'; column++)
+                {
+                    Console.WriteLine($"The cell is ({row}, {column}) (for in for)");
+                }
+            }
+        }
+    }
+}
