34db53cfdf437c8c33e5e4ad0e004667d1862f70
Michał Tracichleb <Michal.Tracichleb@gmail.com>
2022-08-29 22:05:30 +0200

    initial console-task-4

---
+++ console-task-4/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace console_task_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var names = new List<string> { "<name>", "Ana", "Felipe" };
+            foreach (var name in names)
+            {
+                Console.WriteLine($"Hello {name.ToUpper()}!");
+            }
+        }
+    }
+}
