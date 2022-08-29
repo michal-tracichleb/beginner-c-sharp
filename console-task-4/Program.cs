11eacddd5cffada718468fb4755366e431938d16
Michał Tracichleb <Michal.Tracichleb@gmail.com>
2022-08-29 22:17:44 +0200

    added list

--- console-task-4/Program.cs
+++ console-task-4/Program.cs
@@ -1,14 +1,31 @@
-﻿namespace console_task_4
+﻿using System;
+using System.Collections.Generic;
+using System.Xml.Linq;
+
+namespace console_task_4
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            var names = new List<string> { "<name>", "Ana", "Felipe" };
-            foreach (var name in names)
+            var names = new List<string> { "Michał", "Ana", "Felipe" };
+            foreach (var name in names) // wyrzuca wszystkie imiona z listy
             {
                 Console.WriteLine($"Hello {name.ToUpper()}!");
             }
+            Console.WriteLine($"The list has {names.Count} people in it");
+
+            Console.WriteLine("============================");
+
+            names.Add("Piotrek");
+            names.Add("Przemek");
+            names.Remove("Ana");
+
+            for (int i = 0; i  < names.Count; i++)
+            {
+                Console.WriteLine($"Hello {names[i].ToUpper()}!"); // liczymy od 0 w tablicy
+            }
+            Console.WriteLine($"The list has {names.Count} people in it");
         }
     }
 }
